e37a801c90086797aaa6e1454c7f41cfc06715b4
Dilan Dilruksha <100220079+dilrukshax@users.noreply.github.com>
2024-03-14 12:06:42 +0530

    Update config.php

--- SRC/config.php
+++ SRC/config.php
@@ -1,5 +1,5 @@
 <?php 
  
- $con = mysqli_connect("localhost","root","Charuka2001","tutorial") or die("Couldn't connect");
+ $con = mysqli_connect("localhost","root","","") or die("Couldn't connect");
 
 ?>
